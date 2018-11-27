1e8287c54d04510ff37d74a02d135f24bc09fee1
Fernando Pelaez T <fernando.pelaez@cwc.com>
2018-11-27 10:20:33 -0500

    Add file

---
+++ Python/simple_message.py
@@ -0,0 +1,21 @@
+msg = 'Old message'
+print(msg)
+
+msg = "New message"
+print(msg)
+
+name = "ferNando pelAez"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+name = "ferNando"
+lastname = "pelAez"
+full_name = name + " " + lastname
+print("\t" + full_name.title())
+print("\nEnd")
+
+badname = " fernando "
+print("Delete right space: " + badname.rstrip())
+print("Delete left space: " + badname.lstrip())
+print("Delete both spaces: " + badname.strip())
