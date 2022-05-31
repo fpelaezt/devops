943c3f81a18adbc68a5b265054fb4e2f385ea8d8
Fernando Pelaez <fpelaezt@gmail.com>
2022-05-31 14:01:50 -0500

    Submit last adjustments

--- Angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ Angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGIFResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,23 +13,35 @@ export class GifsService {
     return [...this._historial]
   }
 
+  private servicioUrl: string = 'http://api.giphy.com/v1/gifs'
   private apiKeyGiphy: string =  'uCP4hsdj95il9vgU2C89D6OR1oQBcryP'
 
-  // TODO: Cambiar Tipado
-  public resultados: any[] = []
+  public resultados: Gif[] = []
 
-  constructor (private http: HttpClient) {}
+  constructor (private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || []
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || []
+  }
 
   buscarGifs(query: string) {
     query = query.trim().toLowerCase()
     if (query.trim().length === 0) { return }
-    if (this._historial.includes(query)) { return }
-    this._historial.unshift(query)
-    this._historial = this._historial.slice(0, 9)
 
-    this.http.get(`http://api.giphy.com/v1/gifs/search?api_key=${this.apiKeyGiphy}&q=${query}&limit=10`)
-      .subscribe((resp: any) => {
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query)
+      this._historial = this._historial.slice(0, 9)
+      localStorage.setItem('historial', JSON.stringify(this._historial))
+    }
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKeyGiphy)
+      .set('limit', 10)
+      .set('q', query)
+
+    this.http.get<SearchGIFResponse>(`${this.servicioUrl}/search`, { params })
+      .subscribe(resp => {
         this.resultados = resp.data
+        localStorage.setItem('resultados', JSON.stringify(this.resultados))
       })
   }
 }
