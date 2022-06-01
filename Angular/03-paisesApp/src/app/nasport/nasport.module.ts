d49d6553adc308cdd99cf4e100411ba5d184074a
Fernando Pelaez <fpelaezt@gmail.com>
2022-06-01 16:38:02 -0500

    Add angular paises app

---
+++ Angular/03-paisesApp/src/app/nasport/nasport.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NodeComponent } from './node/node.component';
+
+
+
+@NgModule({
+  declarations: [
+    NodeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NasportModule { }
---
+++ Angular/03-paisesApp/src/app/nasport/node/node.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-node',
+  templateUrl: './node.component.html',
+  styles: [
+  ]
+})
+export class NodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
